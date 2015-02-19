3ebb210a09469522d07cd0e078d93dfe6b205877
rajaraman <rajaraman.ks@gmail.com>
2015-02-19 13:08:28 +0530

    Modified for card layout

--- www/js/ui-helper.js
+++ www/js/ui-helper.js
@@ -1,49 +1,55 @@
 //assuming this comes from an ajax call
-var info = [{
+var newsList = [{
     "id": 0,
     "title": "The truth about NASA’s moon exploration",
-    "details": "Fact: About 46 years ago, NASA detected two lifeforms on the Moon. These lifeforms were able to use sophisticated technology, as NASA could easily verify from their audio and video transmissions that we intercepted. I’ve actually seen the video, and the beings seemed humanoid to me, but quite frankly, I think it’s hard to say for sure because of the bulky protective shielding they were wearing at all times."
+    "summary": "About 46 years ago, NASA detected two lifeforms on the Moon. These lifeforms were able to use sophisticated technology, as NASA could easily verify from their audio and video transmissions that we intercepted. I’ve actually seen the video, and the beings seemed humanoid to me, but quite frankly, I think it’s hard to say for sure because of the bulky protective shielding they were wearing at all times",
+    "detail": "About 46 years ago, NASA detected two lifeforms on the Moon. These lifeforms were able to use sophisticated technology, as NASA could easily verify from their audio and video transmissions that we intercepted. I’ve actually seen the video, and the beings seemed humanoid to me, but quite frankly, I think it’s hard to say for sure because of the bulky protective shielding they were wearing at all times"
 }, {
     "id": 1,
     "title": "The truth about NASA’s moon exploration",
-    "details": "Fact: About 46 years ago, NASA detected two lifeforms on the Moon. These lifeforms were able to use sophisticated technology, as NASA could easily verify from their audio and video transmissions that we intercepted. I’ve actually seen the video, and the beings seemed humanoid to me, but quite frankly, I think it’s hard to say for sure because of the bulky protective shielding they were wearing at all times."
+    "summary": "About 46 years ago, NASA detected two lifeforms on the Moon. These lifeforms were able to use sophisticated technology, as NASA could easily verify from their audio and video transmissions that we intercepted. I’ve actually seen the video, and the beings seemed humanoid to me, but quite frankly, I think it’s hard to say for sure because of the bulky protective shielding they were wearing at all times",
+    "detail": "About 46 years ago, NASA detected two lifeforms on the Moon. These lifeforms were able to use sophisticated technology, as NASA could easily verify from their audio and video transmissions that we intercepted. I’ve actually seen the video, and the beings seemed humanoid to me, but quite frankly, I think it’s hard to say for sure because of the bulky protective shielding they were wearing at all times."
 }];
 
-//pageinit event for first page
-//triggers only once
-//write all your on-load functions and event handlers pertaining to page1
-$(document).on("pageinit", "#info-page", function () {
+// pageinit event for first page
+// triggers only once
+$(document).on("pageinit", "#news-list-page", function () {
 
+    //// Set up string for adding <li/>
+    //var li = "";
+    //
+    //// Container for $li to be added
+    //$.each(newsList, function (index, newsItem) {
+    //    //add the <li> to "li" variable
+    //    //note the use of += in the variable
+    //    //meaning I'm adding to the existing data. not replacing it.
+    //    //store index value in array as id of the <a> tag
+    //    //li += '<li><a href="#" id="' + index + '" class="news-item">' + newsItem.summary + '</a></li>';
+    //    li += '<li>' + newsItem.summary + '</li>';
+    //});
+    //
+    //// Append list to ul
+    //$("#news-list-view").append(li).promise().done(function () {
+    //    // Wait for append to finish - thats why you use a promise()
+    //    // done() will run after append is done
+    //    // add the click event for the redirection to happen to #details-page
+    //    $(this).on("click", ".info-go", function (e) {
+    //        e.preventDefault();
+    //
+    //        //store the information in the next page's data
+    //        $("#details-page").data("info", info[this.id]);
+    //        //change the page # to second page.
+    //        //Now the URL in the address bar will read index.html#details-page
+    //        //where #details-page is the "id" of the second page
+    //        //we're gonna redirect to that now using changePage() method
+    //        $.mobile.changePage("#details-page");
+    //    });
+    //
+    //    //refresh list to enhance its styling.
+    //    $(this).listview("refresh");
+    //});
 
-    //set up string for adding <li/>
-    var li = "";
-    //container for $li to be added
-    $.each(info, function (i, name) {
-        //add the <li> to "li" variable
-        //note the use of += in the variable
-        //meaning I'm adding to the existing data. not replacing it.
-        //store index value in array as id of the <a> tag
-        li += '<li><a href="#" id="' + i + '" class="info-go">' + name.name + '</a></li>';
-    });
-    //append list to ul
-    $("#prof-list").append(li).promise().done(function () {
-        //wait for append to finish - thats why you use a promise()
-        //done() will run after append is done
-        //add the click event for the redirection to happen to #details-page
-        $(this).on("click", ".info-go", function (e) {
-            e.preventDefault();
-            //store the information in the next page's data
-            $("#details-page").data("info", info[this.id]);
-            //change the page # to second page.
-            //Now the URL in the address bar will read index.html#details-page
-            //where #details-page is the "id" of the second page
-            //we're gonna redirect to that now using changePage() method
-            $.mobile.changePage("#details-page");
-        });
-
-        //refresh list to enhance its styling.
-        $(this).listview("refresh");
-    });
+    createCards();
 });
 
 //use pagebeforeshow
@@ -67,3 +73,78 @@ $(document).on("pagebeforeshow", "#details-page", function () {
     //add this to html
     $(this).find("[data-role=content]").html(info_view);
 });
+
+
+// function to create the cards
+function createCards() {
+
+    // declaring some variables
+    var uiBlockA = $('#uiBlockA'), // cache the selector of the element, increases performance
+        uiBlockB = $('#uiBlockB'), // cache the selector of the element, increases performance
+        uiBlockC = $('#uiBlockC'), // cache the selector of the element, increases performance
+        i,
+        j = 1,
+        tag,
+
+    // the cards, in this example in an array
+        imagename = ["Image1", "Image2", "Image3"],
+        imagesource = ["http://farm6.staticflickr.com/5323/9902848784_cbd10ba3ca_c.jpg",
+                        "http://farm6.staticflickr.com/5323/9902848784_cbd10ba3ca_c.jpg",
+                        "http://farm6.staticflickr.com/5323/9902848784_cbd10ba3ca_c.jpg"],
+        title = ["Title", "Title", "Title"],
+        text = ["The truth about NASA’s moon exploration", "Lorem ipsum part 2...", "Lorem ipsum part 3..."];
+
+    // emptying the current grid
+    uiBlockA.empty();
+    uiBlockB.empty();
+    uiBlockC.empty();
+
+    // the loop to get the values from the arrays
+    for (i = 0; i < 3; i = i + 1) {
+        tag = '<div class="card" id="card' + i + '">' +
+        '<div class="card-image"><img alt="' + imagename[i] + '" src="' + imagesource[i] + '" />' +
+        '<h2>' + title[i] + '</h2></div>' +
+        '<p>' +  text[i] + '</p>' +
+        '</div>';
+
+        console.log(tag);
+
+        /*	You will need to create cards in a special order.
+         The first 1/3 of the cards are placed in block A.
+         The second 1/3 of the cards are placed in block B.
+         The last 1/3 of the cards are placed in block C.
+
+         This will make sure that the cards will fill white spots
+         when the screen is changing orientation and/or size.
+
+         When you create new block for every card you would get
+         an interface that is lined like a table.
+         */
+        //if (i < (result.rows.length / 3)) {
+        //    exampleBlockA.append(tag);
+        //} else if (i < ((result.rows.length / 3) * 2)) {
+        //    exampleBlockB.append(tag);
+        //} else if (i <= ((result.rows.length / 3) * 3)) {
+        //    exampleBlockC.append(tag);
+        //}
+
+        uiBlockA.append(tag);
+
+        // add a press effect to the card
+        pressEffectCard('card' + i);
+    }
+}
+
+// press effect card ui
+function pressEffectCard(x) {
+    var id = $("#" + x); // cache the selector of the element, increases performance
+    id.off('touchstart').on('touchstart', function () {
+        id.addClass("holoPressEffectDiv");
+    });
+    id.off('touchend').on('touchend', function () {
+        id.removeClass("holoPressEffectDiv");
+    });
+    id.off('touchmove').on('touchmove', function () {
+        id.removeClass("holoPressEffectDiv"); // to remove the press effect when there is a scroll detected in stead of a tap
+    });
+}
