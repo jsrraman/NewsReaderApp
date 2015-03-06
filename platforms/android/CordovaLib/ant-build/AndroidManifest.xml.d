b5f4a30cc9fcd8589e172df889c192ad6e8a4583
Rajaraman <rajaraman.ks@gmail.com>
2015-03-06 12:07:48 +0530

    Removed unnecessary files

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/rajaraman/Documents/Projects/NewsReaderApp/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/rajaraman/Documents/Projects/NewsReaderApp/platforms/android/CordovaLib/AndroidManifest.xml \
